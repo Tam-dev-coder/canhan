0270c1dda454e28c780f872c861359df6fafaf91
Tam <tamdeptrai9763@gmail.com>
2025-10-07 13:49:17 +0700

    cau 1 bai 1

---
+++ bai1/bai1.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main () {
+	long long a, b;
+	cin >> a >> b;
+	long long chianguyen = a / b;
+	long long chiadu = a % b; 
+	cout << chianguyen << " " << chiadu; 
+	return 0; 
+} 
